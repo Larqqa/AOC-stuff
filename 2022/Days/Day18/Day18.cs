989e82defc2629d37da99f6672568f0829f344db
Teemu Nurmi <32098438+Larqqa@users.noreply.github.com>
2024-07-12 12:21:26 +0300

    Day 18 2022 solution for p2 and refactor

--- 2022/Days/Day18/Day18.cs
+++ 2022/Days/Day18/Day18.cs
@@ -6,50 +6,35 @@ namespace _2022.Days.Day18
     [TestClass]
     public class Day18Tests
     {
-        string input = $"2,2,2\r\n1,2,2\r\n3,2,2\r\n2,1,2\r\n2,3,2\r\n2,2,1\r\n2,2,3\r\n2,2,4\r\n2,2,6\r\n1,2,5\r\n3,2,5\r\n2,1,5\r\n2,3,5";
-        Day18 d = new Day18();
+        private const string Input = "2,2,2\r\n1,2,2\r\n3,2,2\r\n2,1,2\r\n2,3,2\r\n2,2,1\r\n2,2,3\r\n2,2,4\r\n2,2,6\r\n1,2,5\r\n3,2,5\r\n2,1,5\r\n2,3,5";
+        private readonly Day18 _d = new();
 
         [TestMethod]
         public void TestP1()
         {
-            d.ParseInput(input);
-            var count = 0;
-            foreach (var c in d.cubes)
-            {
-                count += d.CountTouchingSides(c);
-            }
-            Assert.AreEqual(64, count);
+            _d.ParseInput(Input);
+            Assert.AreEqual(64, _d.FindTotalVolume());
         }
 
         [TestMethod]
         public void TestP2()
         {
-            var input = General.GetInput(@"./Days/Day18/input.txt");
-            d.ParseInput(input);
-            var count = d.FindExteriorVolume();
-            Assert.AreEqual(58, count);
+            _d.ParseInput(Input);
+            Assert.AreEqual(58, _d.FindExteriorVolume());
         }
     }
 
     public class Day18
     {
-        public HashSet<Point> cubes = new();
-        public readonly Point[] matrix = new Point[6]
+        public HashSet<Point> Cubes = new();
+        public readonly Point[] Matrix = new Point[6]
         {
-            new Point( -1, 0, 0 ),
-            new Point( 1, 0, 0 ),
-            new Point( 0, -1, 0 ),
-            new Point( 0, 1, 0 ),
-            new Point( 0, 0, -1 ),
-            new Point( 0, 0, 1 )
-        };
-
-        public readonly Point[] matrixNoZ = new Point[4]
-        {
-            new Point( -1, 0, 0 ),
-            new Point( 1, 0, 0 ),
-            new Point( 0, -1, 0 ),
-            new Point( 0, 1, 0 ),
+            new( -1,  0,  0 ),
+            new(  1,  0,  0 ),
+            new(  0, -1,  0 ),
+            new(  0,  1,  0 ),
+            new(  0,  0, -1 ),
+            new(  0,  0,  1 )
         };
 
         public static void Run()
@@ -60,64 +45,114 @@ namespace _2022.Days.Day18
             Console.WriteLine("------------");
         }
 
-        public void Operation()
+        private void Operation()
         {
-            var input = General.GetInput(@"./Days/Day18/input.txt");
+            var input = General.GetInput("./Days/Day18/input.txt");
             ParseInput(input);
-            var count = 0;
-            foreach(var c in cubes)
-            {
-                count += CountTouchingSides(c);
-            }
-            Console.WriteLine($"non touching sides: {count}");
+            Console.WriteLine($"non touching sides: {FindTotalVolume()}");
+            Console.WriteLine($"exterior volume: {FindExteriorVolume()}");
         }
 
         public void ParseInput(string input)
         {
-            cubes.Clear();
+            Cubes.Clear();
             foreach (var c in input.Replace("\r", "").Split('\n'))
             {
-                cubes.Add(new Point(c));
+                Cubes.Add(new Point(c));
             }
         }
 
-        public int CountTouchingSides(Point p)
+        public int FindTotalVolume()
         {
             var count = 0;
-            foreach(var m in matrix)
+            foreach (var c in Cubes)
+            {
+                count += NonTouchingSides(c);
+            }
+
+            return count;
+        }
+
+        private int NonTouchingSides(Point p)
+        {
+            var count = 0;
+            foreach(var m in Matrix)
             {
                 var side = p.Add(m);
-                if (!cubes.Contains(side)) count++;
+                if (!Cubes.Contains(side)) count++;
             }
             return count;
         }
 
         public int FindExteriorVolume()
         {
-            // Get lists of points by same X,Y value
-            var dict = new Dictionary<Point, List<Point>>();
-            foreach(var c in cubes)
+            // Cube is confined between 0-19, add 1 step of padding on all sides
+            const int size = 22;
+            var cube = new List<string[]>(size);
+            const string rock = "#";
+            const string outside = " ";
+
+            for (var i = 0; i < size; i++)
             {
-                var xy = new Point(c.X, c.Y);
-                if (dict.ContainsKey(xy))
-                {
-                    dict[xy].Add(c);
-                }
-                else
-                {
-                    dict.Add(xy, new() { c });
-                }
+                cube.Add(new string[size * size]);
             }
 
-            // Sort each list from smallest to largest
-            foreach(var e in dict)
+            foreach (var point in Cubes)
             {
-                e.Value.Sort((a, b) => a.Z.CompareTo(b.Z));
+                var p = point.Add(new Point(1,1,1));
+                cube[p.Z][p.ToIndex(size)] = rock;
             }
 
-
+            var visited = new HashSet<Point>();
+            var toCheck = new Queue<Point>();
+            toCheck.Enqueue(new Point());
             var count = 0;
+
+            while (toCheck.Count > 0)
+            {
+                var p = toCheck.Dequeue();
+                if (!visited.Add(p)) continue;
+
+                foreach (var neighbor in Matrix)
+                {
+                    var n = p.Add(neighbor);
+
+                    if (n.OutOfBounds(size, size, size)) continue;
+
+                    var nv = cube[n.Z][n.ToIndex(size)];
+                    if (nv == rock)
+                    {
+                        count++;
+                        continue;
+                    }
+
+                    cube[n.Z][n.ToIndex(size)] = outside;
+                    toCheck.Enqueue(n);
+                }
+
+            }
+
+            //PrintCube(cube, size);
             return count;
+        }
+
+        private static void PrintCube(IReadOnlyList<string[]> cube, int size)
+        {
+            for (var z = 0; z < size; z++)
+            {
+                for (var x = 0; x < size; x++)
+                {
+                    for (var y = 0; y < size; y++)
+                    {
+                        var point = cube[z][y * size + x];
+
+                        Console.Write(string.IsNullOrEmpty(point) ? "o" : point);
+                    }
+
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

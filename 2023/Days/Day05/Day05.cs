2353ccbaccc7bb37c814aee575afe1e7c7c24dcf
Teemu Nurmi <32098438+Larqqa@users.noreply.github.com>
2024-08-02 11:12:16 +0300

    2023 day 5 solutions refactored

--- 2023/Days/Day05/Day05.cs
+++ 2023/Days/Day05/Day05.cs
@@ -19,6 +19,20 @@ public class Day05Tests
 
         Assert.AreEqual("35", d.PartOne());
     }
+
+    [TestMethod]
+    public void TestP2()
+    {
+        var d = new Day05()
+        {
+            Input =
+                "seeds: 79 14 55 13\r\n\r\nseed-to-soil map:\r\n50 98 2\r\n52 50 48\r\n\r\nsoil-to-fertilizer map:\r\n0 15 37\r\n37 52 2\r\n39 0 15\r\n\r\nfertilizer-to-water map:\r\n49 53 8\r\n0 11 42\r\n42 0 7\r\n57 7 4\r\n\r\nwater-to-light map:\r\n88 18 7\r\n18 25 70\r\n\r\nlight-to-temperature map:\r\n45 77 23\r\n81 45 19\r\n68 64 13\r\n\r\ntemperature-to-humidity map:\r\n0 69 1\r\n1 0 69\r\n\r\nhumidity-to-location map:\r\n60 56 37\r\n56 93 4"
+        };
+
+        d.ParseInput();
+
+        Assert.AreEqual("46", d.PartTwo());
+    }
 }
 
 public class Day05() : Day("05")
@@ -73,19 +87,25 @@ public class Day05() : Day("05")
 
     public override string PartOne()
     {
-        var lowest = long.MaxValue;
-
-        foreach (var seed in Seeds)
-        {
-            var location = Maps.GetValueForType(seed, Maps, Maps.MapType.Location);
-            if (location < lowest) lowest = location;
-        }
-
-        return lowest.ToString();
+        return Seeds
+            .Select(seed => Maps.GetValueForType(seed, Maps, Maps.MapType.Location))
+            .Min()
+            .ToString();
     }
 
     public override string PartTwo()
     {
-        throw new NotImplementedException();
+        var lowest = long.MaxValue;
+        for (var i = 0; i < Seeds.Count; i += 2)
+        {
+            var seed = Seeds[i];
+            var amount = Seeds[i + 1] - 1;
+            var mapped = Maps.GetValueForType((seed, seed + amount), Maps, Maps.MapType.Location);
+
+            var lowestLocation = mapped.Min(x => x.start);
+            if (lowestLocation < lowest) lowest = lowestLocation;
+        }
+
+        return lowest.ToString();
     }
 }
--- 2023/Days/Day05/Maps.cs
+++ 2023/Days/Day05/Maps.cs
@@ -2,36 +2,37 @@
 
 public class Maps
 {
-    public List<(long destinationStart, long SourceStart, long Length)> SeedToSoil = [];
-    public List<(long destinationStart, long SourceStart, long Length)> SoilToFertilizer = [];
-    public List<(long destinationStart, long SourceStart, long Length)> FertilizerToWater = [];
-    public List<(long destinationStart, long SourceStart, long Length)> WaterToLight = [];
-    public List<(long destinationStart, long SourceStart, long Length)> LightToTemperature = [];
-    public List<(long destinationStart, long SourceStart, long Length)> TemperatureToHumidity = [];
-    public List<(long destinationStart, long SourceStart, long Length)> HumidityToLocation = [];
+    public List<(long DestinationStart, long SourceStart, long Length)>? SeedToSoil = [];
+    public List<(long DestinationStart, long SourceStart, long Length)>? SoilToFertilizer = [];
+    public List<(long DestinationStart, long SourceStart, long Length)>? FertilizerToWater = [];
+    public List<(long DestinationStart, long SourceStart, long Length)>? WaterToLight = [];
+    public List<(long DestinationStart, long SourceStart, long Length)>? LightToTemperature = [];
+    public List<(long DestinationStart, long SourceStart, long Length)>? TemperatureToHumidity = [];
+    public List<(long DestinationStart, long SourceStart, long Length)>? HumidityToLocation = [];
+    private static readonly MapType LastMapType = Enum.GetValues(typeof(MapType)).Cast<MapType>().Last();
 
-    public static long GetValueFromRange(long val, (long destinationStart, long SourceStart, long Length) map)
+    public static long GetValueFromRange(long seed, (long DestinationStart, long SourceStart, long Length) map)
     {
-        if (val < map.SourceStart || val > map.SourceStart + map.Length - 1) return val;
+        var (start, end) = GetSourceBounds(map);
+        if (seed < start || end < seed) return seed;
 
-        return val + map.destinationStart - map.SourceStart;
+        return seed + map.DestinationStart - map.SourceStart;
     }
 
-    public static long GetValueFromMap(long val, List<(long destinationStart, long SourceStart, long Length)>? map)
+    public static long GetValueFromMap(long seed, List<(long DestinationStart, long SourceStart, long Length)>? map)
     {
-        if (map == null) return val;
+        if (map == null) return seed;
 
-        var res = val;
         foreach (var range in map)
         {
-            var newVal = GetValueFromRange(val, range);
-            if (val != newVal) res = newVal;
+            var newVal = GetValueFromRange(seed, range);
+            if (seed != newVal) return newVal;
         }
 
-        return res;
+        return seed;
     }
 
-    public static List<(long destinationStart, long SourceStart, long Length)>? GetMapForType(MapType type, Maps map)
+    public static List<(long DestinationStart, long SourceStart, long Length)>? GetMapForType(MapType type, Maps map)
     {
         return type switch
         {
@@ -51,13 +52,90 @@ public class Maps
     {
         while (true)
         {
-            var m = GetMapForType(current, maps);
-            var val = GetValueFromMap(seed, m);
+            var map = GetMapForType(current, maps);
+            var value = GetValueFromMap(seed, map);
 
-            if (current == target) return val;
-
-            seed = val;
             current++;
+            if (current == target) return value;
+            if (current > LastMapType) throw new Exception("Outside of map type range!");
+
+            seed = value;
+        }
+    }
+
+    public static (long start, long end) GetSourceBounds((long DestinationStart, long SourceStart, long Length) range)
+    {
+        return (range.SourceStart, range.SourceStart + range.Length - 1);
+    }
+
+    public static List<(long start, long end)> SplitRange((long start, long end) seed, List<(long DestinationStart, long SourceStart, long Length)>? map)
+    {
+        var ranges = new List<(long start, long end)>();
+        if (map == null) return ranges;
+
+        foreach (var range in map)
+        {
+            var bounds = GetSourceBounds(range);
+            var offset = range.DestinationStart - range.SourceStart;
+
+            // Completely inside
+            if (bounds.start <= seed.start && seed.end <= bounds.end)
+            {
+                ranges.Add((seed.start + offset, seed.end + offset));
+                return ranges;
+            };
+
+            // Completely outside of range
+            if (bounds.end < seed.start || seed.end < bounds.start) continue;
+
+            // Starts outside, ends inside
+            if (seed.start < bounds.start && seed.end <= bounds.end)
+            {
+                ranges.Add((bounds.start + offset, seed.end + offset));
+                seed.end = bounds.start - 1;
+            }
+            
+            // Starts inside, ends outside
+            if (bounds.start <= seed.start && bounds.end < seed.end)
+            {
+                ranges.Add((seed.start + offset, bounds.end + offset));
+                seed.start = bounds.end + 1;
+            }
+        }
+
+        ranges.Add(seed); // Unmodified seeds outside of map ranges
+        return ranges;
+    }
+
+    public static List<(long start, long end)> GetMappedRanges((long start, long end) seedRanges, MapType type, Maps? maps)
+    {
+        var mappedRanges = new List<(long start, long end)>();
+        if (maps == null) return mappedRanges;
+
+        var map = GetMapForType(type, maps);
+        if (map == null) return mappedRanges;
+
+        return SplitRange(seedRanges, map);
+    }
+
+    public static List<(long start, long end)> GetValueForType((long start, long end) seed, Maps maps, MapType target, MapType current = MapType.Seed)
+    {
+        var ranges = new List<(long start, long end)> { seed };
+
+        while (true)
+        {
+            var temp = new List<(long start, long end)>();
+            foreach (var range in ranges)
+            {
+                var mappedRanges = GetMappedRanges(range, current, maps);
+                temp = [.. temp, .. mappedRanges];
+            }
+
+            ranges = temp;
+
+            current++;
+            if (current == target) return ranges;
+            if (current > LastMapType) throw new Exception("Outside of map type range!");
         }
     }
 
